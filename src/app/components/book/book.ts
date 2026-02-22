989beb2e43d7c448ab63a30aaca56323e1002568
bitelon <daniel.trivicevic@gmail.com>
2026-02-22 21:59:42 +0100

    add formControl for book input

--- src/app/components/book/book.ts
+++ src/app/components/book/book.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BookFormDto } from '../../dto/bookDto';
 
 @Component({
   selector: 'book-form',
@@ -8,11 +9,36 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './book.scss',
 })
 export class Book {
+    @Output() saveBookEvent = new EventEmitter<BookFormDto>();
+    submitted = false;
+
+
     bookForm = new FormGroup({
-      name: new FormControl(),
-      author: new FormControl(),
-      note: new FormControl()
+      name: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      author: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(3)]
+    }),
+      note: new FormControl<string>('', { nonNullable: true }),
     })
 
+    saveBook() {
+      console.log('save Book');
+      this.submitted = true;
+      if(this.bookForm.invalid) {
+        this.bookForm.markAllAsTouched();
+        return;
+      }
 
+      const bookData: BookFormDto = {
+        name: this.bookForm.controls.name.value,
+        author: this.bookForm.controls.author.value,
+        note: this.bookForm.controls.note.value,
+      };
+
+      this.saveBookEvent.emit(bookData);
+    }
 }
