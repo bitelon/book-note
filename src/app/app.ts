ca494259a5049b2a00ef1eda00a0990e970eb2c2
bitelon <daniel.trivicevic@gmail.com>
2026-03-11 21:59:37 +0100

    reset whole project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Book } from "./components/book/book";
-import { BookNote } from "./components/book-note/book-note";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Book, BookNote],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('book-note');
-}
+export class App {}
